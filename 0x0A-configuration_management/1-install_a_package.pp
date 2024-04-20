cbd52903599a6fbdccd435264a69b4596f9abb3a
rash0x6964 <rash0x6964@gmail.com>
2024-04-20 07:04:37 +0100

    trying to find out why flask cmd not working

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
 # install flask from pip3
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3provider',
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+  path    => '/usr/local/bin',
+  notify  => Exec['refresh_shell'],
+}
+
+exec { 'refresh_shell':
+  command     => 'exec bash -l',
+  refreshonly => true,
 }
