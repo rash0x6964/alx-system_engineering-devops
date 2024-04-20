df609b0078ee3ebfe6ada7cac2cd231768dd22d2
rash0x6964 <rash0x6964@gmail.com>
2024-04-20 07:16:19 +0100

    it work's on my cp :)

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,12 +2,5 @@
 
 exec { 'install_flask':
   command => '/usr/bin/pip3 install flask==2.1.0',
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
-  path    => '/usr/local/bin',
-  notify  => Exec['refresh_shell'],
-}
-
-exec { 'refresh_shell':
-  command     => 'exec bash -l',
-  refreshonly => true,
+  unless  => '/usr/bin/pip3 show flask | /bin/grep -q "Version: 2.1.0"',
 }
