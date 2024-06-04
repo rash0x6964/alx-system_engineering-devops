959aa352b9bce44305203b7cdd611b4c9e2af7e7
rash0x6964 <rash0x6964@gmail.com>
2024-06-04 13:03:38 +0100

    How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""How many subs"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """returns the number of subscribers for a given subreddit"""
+    
+    headers = {'User-Agent': '0x16-api_advanced-jmajetich'}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+
+    try:
+        data = response.json()['data']
+        return data['subscribers']
+    except (KeyError, ValueError):
+        return 0
+
+    pass
