5c5fc41628c33a7cbddfee40a76f5a0c5d9bc7ee
rash0x6964 <rash0x6964@gmail.com>
2024-06-11 16:52:06 +0100

    Sky is the limit, let's bring that limit higher

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,7 @@
+#Puppet file that fixes multiple server errors in Nginx server
+
+exec { 'fix_nginx_ulimit':
+  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command  =>  "sed -i 's/ULIMIT=\"-n 15\"/ULIMIT=\"-n 3072\"/g' /etc/default/nginx; sudo service nginx restart",
+  provider =>  'shell'
+}
